a66db8365b19acd74e4b2ab41f6860a0a496d98f
Deovrat Singh <devsingh6n@gmail.com>
2020-08-08 01:07:33 +0530

    Youtube Video Library Project Setup

---
+++ corelib/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import CoreLib, Video
+
+
+admin.site.register(CoreLib)
+admin.site.register(Video)
+# Register your models here.
---
+++ corelib/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CorelibConfig(AppConfig):
+    name = 'corelib'
