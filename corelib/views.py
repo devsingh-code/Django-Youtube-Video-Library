283835ae3d8d9059f26a5a0905efdace851b558e
Deovrat Singh <devsingh6n@gmail.com>
2020-08-08 17:55:45 +0530

    Added CRUD views and Add Video View

--- corelib/views.py
+++ corelib/views.py
@@ -4,9 +4,19 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from .models import VideoLib
 from django.contrib.auth import authenticate, login
+from .forms import VideoForm
+
 # Create your views here.
 def home(request):
-    return render(request, 'corelib\home.html')
+    return render(request, 'corelib/home.html')
+
+def dashboard(request):
+    return render(request,'corelib/dashboard.html')
+
+def add_video(request, pk):
+    form = VideoForm()
+    return render(request, 'corelib/add_video.html',{'form':form})
+
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
@@ -32,6 +42,7 @@ class SignUp(generic.CreateView):
     #     # create a form for a hall
     #     # return template
 
+#CRUD
 class CreateVideoLib(generic.CreateView):
     model = VideoLib
     fields = ['title']
@@ -41,9 +52,22 @@ class CreateVideoLib(generic.CreateView):
     def form_valid(self,form):
         form.instance.user = self.request.user
         super(CreateVideoLib,self).form_valid(form)
-        return redirect('home')
+        return redirect('dashboard')
 
+class DetailVideoLib(generic.DetailView):
+    model = VideoLib
+    template_name = 'corelib/detail_videolib.html'
 
+class UpdateVideoLib(generic.UpdateView):
+    model = VideoLib
+    template_name = 'corelib/update_videolib.html'
+    fields = ['title']
+    success_url = reverse_lazy('dashboard')
+
+class DeleteVideoLib(generic.DeleteView):
+    model = VideoLib
+    template_name = 'corelib/delete_videolib.html'
+    success_url = reverse_lazy('dashboard')
 
 
 
--- videolib/urls.py
+++ videolib/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     #AUTH
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
@@ -30,5 +31,11 @@ urlpatterns = [
 
     #CORELIB
     path('videolib/create', views.CreateVideoLib.as_view(), name='create_videolib'),
+    path('videolib/<int:pk>', views.DetailVideoLib.as_view(), name='detail_videolib'),
+    path('videolib/<int:pk>/update', views.UpdateVideoLib.as_view(), name='update_videolib'),
+    path('videolib/<int:pk>/delete', views.DeleteVideoLib.as_view(), name='delete_videolib'),
+
+    # Video
+    path('videolib/<int:pk>/addvideo', views.add_video, name='add_video'),
 ]
 urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
