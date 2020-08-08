8294c36a22c9564b60030021f0302a7dc058dd89
Deovrat Singh <devsingh6n@gmail.com>
2020-08-08 14:38:14 +0530

    Added auto login functionality post signup

--- corelib/views.py
+++ corelib/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from .models import VideoLib
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def home(request):
     return render(request, 'corelib\home.html')
@@ -11,6 +12,15 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
+
+    def form_valid(self,form):
+        view = super(SignUp,self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
+
+
 
 # def create_videolib(request):
     # if request.method == 'POST':
@@ -27,5 +37,13 @@ class CreateVideoLib(generic.CreateView):
     fields = ['title']
     template_name = 'corelib/create_videolib.html'
     success_url=reverse_lazy('home')
+    
+    def form_valid(self,form):
+        form.instance.user = self.request.user
+        super(CreateVideoLib,self).form_valid(form)
+        return redirect('home')
+
+
+
 
 
